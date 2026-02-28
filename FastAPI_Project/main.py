f0beaa14938e28e932868b63c7d597249b6820d8
Raunak-Dehankar <raunak.42.dehankar@gmail.com>
2026-02-28 20:50:30 +0530

    added ORM funtion and auth.

--- FastAPI_Project/main.py
+++ FastAPI_Project/main.py
@@ -1,108 +1,207 @@
-from fastapi import FastAPI, HTTPException
-import sqlite3
+from fastapi import FastAPI, HTTPException, Depends, Header
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from pydantic import BaseModel
+from passlib.context import CryptContext
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+
+# databse config and creating a base for orm model
+
+DATABASE_URL = "sqlite:///./users.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+# ORM MODEL, sql table is defined here
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+
+Base.metadata.create_all(bind=engine)
+
+# Pydantic Classes (Request/Response), fastapi converts orm abjects to json here. 
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+
+class UserUpdate(BaseModel):
+    name: str
+    email: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+
+    class Config:
+        from_attributes = True
+
+# here we will define our security details 
+
+SECRET_KEY = "vadapav"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain, hashed):
+    return pwd_context.verify(plain, hashed)
+
+
+def create_token(email: str):
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload = {"sub": email, "exp": expire}
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+
 
 #to launch FastAPI and handles the webpage
 app = FastAPI()
 
-# Database setup, creating a table here for the database users
-
-#connecting to database
-conn = sqlite3.connect("users.db", check_same_thread=False)
-cursor = conn.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE
-)
-""")
-conn.commit()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
+# Create user + register (previously just to create new, now it will work only after auth.)
+@app.post("/register", response_model=UserResponse)
+def register(user: UserCreate, db: Session = Depends(get_db)):
 
-# Create user
-@app.post("/users/")
-def create_user(user: dict):
+    existing = db.query(User).filter(User.email == user.email).first()
+    if existing:
+        raise HTTPException(status_code=400, detail="Email already exists")
+
+    new_user = User(
+        name=user.name,
+        email=user.email,
+        password=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+# Login, takes user login cred. 
+
+@app.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user or not verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    token = create_token(db_user.email)
+
+    return {"access_token": token}
+
+
+# This function here verifies the user token and authenticates user as well as each operation
+
+def get_current_user(authorization: str = Header(None), db: Session = Depends(get_db)):
+
+    if not authorization:
+        raise HTTPException(status_code=401, detail="Token missing")
 
     try:
-        cursor.execute(
-            "INSERT INTO users (name, email) VALUES (?, ?)",
-            (user["name"], user["email"])
-        )
-        conn.commit()
+        token = authorization.split(" ")[1]  # Remove "Bearer"
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload.get("sub")
+    except (JWTError, IndexError):
+        raise HTTPException(status_code=401, detail="Invalid token")
 
-        return {"message": "User created successfully"}
+    user = db.query(User).filter(User.email == email).first()
 
-    except sqlite3.IntegrityError:
-        raise HTTPException(status_code=400, detail="Email already exists")
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return user
 
 
 # Read user data, basically get the database
-@app.get("/users/")
-def get_users():
-
-    cursor.execute("SELECT * FROM users")
-    users = cursor.fetchall()
-
-    result = []
-    for user in users:
-        result.append({
-            "id": user[0],
-            "name": user[1],
-            "email": user[2]
-        })
-
-    return result
-
+@app.get("/users/", response_model=list[UserResponse])
+def get_users(current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    return db.query(User).all()
 
 # Read one specific user 
-@app.get("/users/{user_id}")
-def get_user(user_id: int):
+@app.get("/users/{user_id}", response_model=UserResponse)
+def get_user(user_id: int, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
 
-    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
-    user = cursor.fetchone()
+    user = db.query(User).filter(User.id == user_id).first()
 
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    return {
-        "id": user[0],
-        "name": user[1],
-        "email": user[2]
-    }
+    return user
 
 
 # Updatr user
-@app.put("/users/{user_id}")
-def update_user(user_id: int, updated_user: dict):
+@app.put("/users/{user_id}", response_model=UserResponse)
+def update_user(user_id: int, updated_user: UserUpdate,
+                current_user: User = Depends(get_current_user),
+                db: Session = Depends(get_db)):
 
-    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
-    existing_user = cursor.fetchone()
+    user = db.query(User).filter(User.id == user_id).first()
 
-    if not existing_user:
+    if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    cursor.execute(
-        "UPDATE users SET name = ?, email = ? WHERE id = ?",
-        (updated_user["name"], updated_user["email"], user_id)
-    )
-    conn.commit()
+    user.name = updated_user.name
+    user.email = updated_user.email
 
-    return {"message": "User updated successfully"}
+    db.commit()
+    db.refresh(user)
+
+    return user
 
 
 # Delete user
 @app.delete("/users/{user_id}")
-def delete_user(user_id: int):
+def delete_user(user_id: int,
+                current_user: User = Depends(get_current_user),
+                db: Session = Depends(get_db)):
 
-    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
-    user = cursor.fetchone()
+    user = db.query(User).filter(User.id == user_id).first()
 
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    cursor.execute("DELETE FROM users WHERE id = ?", (user_id,))
-    conn.commit()
+    db.delete(user)
+    db.commit()
 
     return {"message": "User deleted successfully"}
+
+# Logout, dunno ye kar kya raha yet
+
+@app.post("/logout")
+def logout():
+    return {"message": "Delete token on client side to logout"}
